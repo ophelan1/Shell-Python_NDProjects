f228094e6f05dd168c9759b0f79176add789bbe6
ophelan <phelan.eoin@gmail.com>
2016-02-29 13:20:28 -0500

    final submit

--- reading07/catalog_summary.sh
+++ reading07/catalog_summary.sh
@@ -8,24 +8,37 @@ else
 fi
 # A better reddit.sh
 curl -s $urlStr | \
-awk 'BEGIN     {
-        FS= "\n\n"
-        count = 0
+awk 'BEGIN {
+		printf "# Begin Section:\n"
+		counter=0
+		totalMachines=0
  	}
- 	{
- 	   	/type/ {
-   			types[$2]++
-   			count++
+ 	/type/ {
+		types[$2]++
+   	}
+   	/name/ {
+   		if ( $2 in names ){
+   			names[$2]=1
    		}
-	}
+   		else {
+   			totalMachines++
+   		}
+   		counter++
+   	}
+   	{
+   		counter++
+   	}
 	END {
-       for (i = 0; i < count; i++) {
-            if (i in Titles) {
-                printf "%2d. %s\n", c, Titles[i]
-                printf "    %s\n\n", URLs[i]
-                c++                                                                                                    
-            }
+		printf "# End Section:\n"
+		i=0
+		max=0
+    	for (i in types) {
+    		if ( types[i] > max ){
+    			max=i
+    		}
         }
-    }	
-}
-'
+        printf "Total Numer of Machines:    %d\n", totalMachines
+        printf "Most Prolific Type:   		%s\n", max
+        printf "Total Number of CPUs :		%d \n ", counter
+
+    }'
